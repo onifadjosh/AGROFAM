9d54132476e31a5a2dfdd35f9717ef36cd68d8a3
onifadjosh <onifadjosh@gmail.com>
2022-12-10 23:49:46 +0100

    first commit on project

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        'primcol': '#007A25',
+        'seccol' : '#FDFDFD',
+      },
+    },
+  },
+  plugins: [],
+}
